90ac65252a33a77000fd8553e1f8279eacf614dd
EvgenyShokotko <qaterfacs@gmail.com>
2022-08-20 21:17:19 +0300

    second attemp

--- js/task-07.js
+++ js/task-07.js
@@ -1,11 +1,12 @@
-const inputEl = document.querySelector('#font-size-control')
-const spanEl = document.querySelector('#text')
+const inputEl = document.querySelector("#font-size-control");
+const spanEl = document.querySelector("#text");
 
 inputEl.addEventListener("input", onChangeTextSize);
-spanEl.style.fontSize = `56px`
+spanEl.style.fontSize = `${inputEl.value}px`;
+console.log(inputEl.value);
 
 function onChangeTextSize(event) {
-    const size = event.currentTarget.value;
-    console.log(size);
-    spanEl.style.fontSize = `${size}px`
+  const size = event.currentTarget.value;
+  console.log(size);
+  spanEl.style.fontSize = `${size}px`;
 }
